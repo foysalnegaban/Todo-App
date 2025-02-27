6794c928d84d5c0455cf9d9eb318d0525a711ad4
foysalnegaban <foysal.bubt022@gmail.com>
2025-02-27 22:12:30 +0600

    first commit

---
+++ app/Http/Controllers/AuthManager.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthManager extends Controller
+{
+    function login(){
+        return view('auth.login');
+    }
+
+    function loginPost(Request $request){
+
+    }
+}
---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield("title", "To Do App")</title>
+    <link href="{{'assets/css/bootstrap.css'}}" rel="stylesheet">
+    @yield("style")
+  </head>
+  <body class="d-flex align-items-center py-4 bg-body-tertiary">
+  
+    @yield("content")
+    
+
+    <script src="assets/js/bootstrap.min.js"></script>
+  </body>
+</html>  
